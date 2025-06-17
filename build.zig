4b42c80650dcb3c9d32023c1ced69c0df074d0d0
Tabre <tabre@tabre.com>
2025-06-17 01:13:38 -0500

    update build files

--- build.zig
+++ build.zig
@@ -5,13 +5,13 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const zapper = b.createModule(.{
+    const zap_srv = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const tooling = b.createModule(.{
+    const zapper = b.createModule(.{
         .root_source_file = b.path("src/tooling/zapper.zig"),
         .target = target,
         .optimize = optimize,
@@ -22,43 +22,43 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .openssl = false,
     }); 
+    zap_srv.addImport("zap", zap.module("zap"));
     zapper.addImport("zap", zap.module("zap"));
-    tooling.addImport("zap", zap.module("zap"));
 
     const clap = b.dependency("clap", .{
         .target = target,
         .optimize = optimize,
     });
-    tooling.addImport("clap", clap.module("clap"));
+    zapper.addImport("clap", clap.module("clap"));
+
+    const exe_zap_srv = b.addExecutable(.{
+        .name = "zap-srv",
+        .root_module = zap_srv,
+    });
+    b.installArtifact(exe_zap_srv);
 
     const exe_zapper = b.addExecutable(.{
-        .name = "zap-srv",
-        .root_module = zapper,
-    });
-
-    const exe_tooling = b.addExecutable(.{
         .name = "zapper",
-        .root_module = tooling
+        .root_module = zapper
     });
-
     b.installArtifact(exe_zapper);
 
+    const run_cmd_zap_srv = b.addRunArtifact(exe_zap_srv);
     const run_cmd_zapper = b.addRunArtifact(exe_zapper);
-    const run_cmd_tooling = b.addRunArtifact(exe_tooling);
 
+    run_cmd_zap_srv.step.dependOn(b.getInstallStep());
     run_cmd_zapper.step.dependOn(b.getInstallStep());
-    run_cmd_tooling.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
+        run_cmd_zap_srv.addArgs(args);
         run_cmd_zapper.addArgs(args);
-        run_cmd_tooling.addArgs(args);
     }
-
-    const run_step_zapper = b.step("run", "Run web server");
-    run_step_zapper.dependOn(&run_cmd_zapper.step);
     
-    const run_step_tooling = b.step("run-zapper", "Run zapper utility");
-    run_step_tooling.dependOn(&run_cmd_tooling.step);
+    const run_step_zap_srv = b.step("run", "Run Zap web server");
+    run_step_zap_srv.dependOn(&run_cmd_zap_srv.step);
+
+    const run_step_zapper= b.step("run-zapper", "Run zapper utility");
+    run_step_zapper.dependOn(&run_cmd_zapper.step);
 
     // const exe_unit_tests = b.addTest(.{
     //     .root_module = exe_mod,
